9a119fd046ff8d5bf8db8cb334791286983f6ec2
dsjian <jiandongshan@hotmail.com>
2026-02-17 21:05:47 +0800

    refactor: for reasoning; CPhO and IPhO results

--- src/api/__init__.py
+++
@@ -1,8 +0,0 @@
-__all__ = [
-    "chat_openai_compatible",
-    "chat_anthropic",
-    "chat_gemini_2_5"
-]
-from .interface_openai import chat_openai_compatible
-from .interface_anthropic import chat_anthropic
-from .interface_gemini import chat_gemini_2_5
